c09fb0b6d77f1452675b4659b3388cd55998ca12
Brian Bartholomew <me@bcbrian.com>
2019-11-14 14:09:05 -0600

    intervals and timeouts

--- scripts.js
+++ scripts.js
@@ -1,22 +1,35 @@
 const buttonEl = document.querySelector("button");
 buttonEl.setAttribute("class", "btn btn-secondary btn-lg");
-console.log("foo");
-function takesReallllllyLongTime(cb) {
-  console.log("bar");
+
+function takesReallllllyLongTime(callBackFunction) {
   /*
   TAKES REALLY LONG TIME TO DO THING
   */
-  cb("the info that you wanted");
+  setTimeout(function() {
+    buttonEl.setAttribute("class", "btn btn-success btn-lg");
+    console.log("done!");
+    callBackFunction("the info that you wanted");
+  }, 10000);
 }
 
-console.log("baz");
 takesReallllllyLongTime(function(info) {
   console.log(info);
   buttonEl.setAttribute("class", "btn btn-danger btn-lg");
 });
+
 console.log("start");
 setTimeout(function() {
   buttonEl.setAttribute("class", "btn btn-success btn-lg");
   console.log("done!");
 }, 5000);
 console.log("done?");
+
+setInterval(function() {
+  buttonEl.setAttribute("class", "btn btn-dark btn-lg");
+  console.log("done!");
+}, 1444);
+
+setInterval(function() {
+  buttonEl.setAttribute("class", "btn btn-light btn-lg");
+  console.log("done!");
+}, 2444);
