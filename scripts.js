2c882be44ba0231a1b4f0771b2887728b026bdee
Brian Bartholomew <me@bcbrian.com>
2019-11-15 14:41:55 -0600

    we can identify a match

--- scripts.js
+++ scripts.js
@@ -10,11 +10,32 @@ function initGame() {
     startContainerEl.setAttribute("class", "container d-none");
     gameContainerEl.setAttribute("class", "container");
 
+    let clicks = [];
+
     // select all the memory buttons
-    // add an event listener for all the memory buttons
-    // define the behavior for the click
-    // if the first click STOP :P
-    // else the second check for equality
+    const tileEls = document.querySelectorAll(".tiles-js");
+    // add an event listener to all the memory buttons
+    tileEls.forEach(function(tileEl) {
+      tileEl.addEventListener("click", function() {
+        console.log(tileEl);
+        // define the behavior for the click
+        clicks.push(tileEl);
+        // if the first click STOP :P
+        if (clicks.length < 2) {
+          return;
+        }
+        // else the second check for equality
+        if (
+          clicks[0] !== clicks[1] &&
+          clicks[0].innerText === clicks[1].innerText
+        ) {
+          alert("winner");
+        }
+
+        // when all done
+        clicks = [];
+      });
+    });
   }
   buttonEl.addEventListener("click", startGame);
 }
