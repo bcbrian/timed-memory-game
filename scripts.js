e94c54f3e63520739e77785cd092e817ae7ba37e
Brian Bartholomew <me@bcbrian.com>
2019-11-15 10:31:05 -0600

    added buttons 4x4,  some js stuff

--- scripts.js
+++ scripts.js
@@ -1,35 +1,10 @@
-const buttonEl = document.querySelector("button");
-buttonEl.setAttribute("class", "btn btn-secondary btn-lg");
+function initGame() {
+  const buttonEl = document.querySelector("button");
+  buttonEl.setAttribute("class", "btn btn-secondary btn-lg");
 
-function takesReallllllyLongTime(callBackFunction) {
-  /*
-  TAKES REALLY LONG TIME TO DO THING
-  */
-  setTimeout(function() {
+  function startGame() {
     buttonEl.setAttribute("class", "btn btn-success btn-lg");
-    console.log("done!");
-    callBackFunction("the info that you wanted");
-  }, 10000);
+  }
+  buttonEl.addEventListener("click", startGame);
 }
-
-takesReallllllyLongTime(function(info) {
-  console.log(info);
-  buttonEl.setAttribute("class", "btn btn-danger btn-lg");
-});
-
-console.log("start");
-setTimeout(function() {
-  buttonEl.setAttribute("class", "btn btn-success btn-lg");
-  console.log("done!");
-}, 5000);
-console.log("done?");
-
-setInterval(function() {
-  buttonEl.setAttribute("class", "btn btn-dark btn-lg");
-  console.log("done!");
-}, 1444);
-
-setInterval(function() {
-  buttonEl.setAttribute("class", "btn btn-light btn-lg");
-  console.log("done!");
-}, 2444);
+initGame();
